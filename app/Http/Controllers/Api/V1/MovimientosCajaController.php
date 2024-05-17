9aba3d9d1492e442a7c086637e9d37b68922f1b3
diana-gomez1 <diana.gomez05@uceva.edu.co>
2024-05-16 22:45:25 -0500

    Se agrega Movimientoscaja en el controller y Models

---
+++ app/Http/Controllers/Api/V1/MovimientosCajaController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\MovimientosCaja;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MovimientosCajaController extends Controller 
+{
+    //obtener todos los registros
+    public function index(Request $request)
+    {
+        $movimientos = MovimientosCaja::all();
+        return response()->json([
+            'status' => true,
+            'movimientos' => $movimientos
+        ]);
+    }
+
+    //crear registro
+    public function store(Request $request)
+    {
+        try {
+            $movimiento = MovimientosCaja::create($request->all());
+
+            return response()->json([
+                'status' => true,
+                'message' => "¡¡Movimiento creado exitosamente!!",
+                'movimiento' => $movimiento
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => "--Error al crear el movimiento--"
+            ], 500);
+        }
+    }
+
+    //obtener registro específico por ID
+    public function show($id)
+    {
+        $movimiento = MovimientosCaja::find($id);
+
+        if (!$movimiento) {
+            return response()->json(['error' => 'El movimiento no existe.'], 404);
+        }
+        return response()->json(['movimiento' => $movimiento]);
+    }
+
+    //actualizar  registro específico por ID
+    public function update(Request $request, $id)
+    {
+        try {
+            $movimiento = MovimientosCaja::find($id);
+
+            if (!$movimiento) {
+                return response()->json(['error' => 'El movimiento no existe.'], 404);
+            }
+            $movimiento->update($request->all());
+            return response()->json([
+                'status' => true,
+                'message' => "Movimiento actualizado correctamente.",
+                'movimiento' => $movimiento
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => "Error al actualizar el movimiento"
+            ], 500);
+        }
+    }
+
+    //eliminar registro específico por ID
+    public function destroy($id)
+    {
+        try {
+            $movimiento = MovimientosCaja::find($id);
+
+            if (!$movimiento) {
+                return response()->json(['error' => 'El movimiento no existe.'], 404);
+            }
+            $movimiento->delete();
+            return response()->json(['message' => 'El movimiento fue eliminado correctamente.']);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => "Error al eliminar el movimiento"
+            ], 500);
+        }
+    }
+}
+
---
+++ app/Models/MovimientosCaja.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB; // Se importa la base de datos para la consulta de secuencias
+
+class MovimientosCaja extends Model
+{
+    use HasFactory;
+
+    protected $table = 'movimientos_caja'; 
+    protected $primaryKey = 'id_caja';
+    public $incrementing = false; //el incremento automatico del id_caja comienza en falso
+    protected $keyType = 'integer';//clave de tipo entero
+    public $timestamps = false;
+    
+
+    protected $fillable = [
+        'descripcion_movimiento',
+        'tipo_movimiento',
+        'monto_pagar',
+    ];
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {//esto se ejecuta antes de que un registro sea insertado en la BD.
+            // Obtener el siguiente valor de la secuencia que comienza en 1 e incrementa de uno en uno.
+            $nextId = DB::selectOne("SELECT nextval('movimientoscaja_id_caja_seq') as id")->id;
+            $model->id_caja = $nextId;//el valor obtenido  se agina a la propiedad id_caja
+        });
+    }
+}
